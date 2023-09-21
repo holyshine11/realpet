048d865b457b0ef694f6c5236ef37e432c9cf97c
scott <holyshine11@naver.com>
2023-09-21 17:07:50 +0900

    Update settings.py

--- DG_PRJ/settings.py
+++ DG_PRJ/settings.py
@@ -15,7 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = os.environ.get("SECRET_KEY", "default_secret_key")
+# "qwer_qwer_qwer"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
